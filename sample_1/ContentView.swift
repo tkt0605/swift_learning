9add617184da9522e0a802214f161371a5e61c53
tkt0605 <takato.komada2005@outlook.jp>
2025-10-13 10:28:14 +0900

    Initial Commit

---
+++ sample_1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sample_1
+//
+//  Created by 駒田隆人 on 2025/10/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sample_1/sample_1App.swift
@@ -0,0 +1,17 @@
+//
+//  sample_1App.swift
+//  sample_1
+//
+//  Created by 駒田隆人 on 2025/10/13.
+//
+
+import SwiftUI
+
+@main
+struct sample_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
